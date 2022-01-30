f016ec89d72f414819f899e8cbcf1f54d123ec18
Michal Artazov <michal@artazov.cz>
2022-01-30 17:54:53 +0100

    set thermostat mode to auto

--- src/service/ThermostatService.ts
+++ src/service/ThermostatService.ts
@@ -5,17 +5,32 @@ import { Characteristic } from './Characteristic';
 const DEFAULT_CURRENT_TEMP = 10;
 const DEFAULT_TARGET_TEMP = 10;
 
+enum TargetHeatingCoolingState {
+	OFF = 0,
+	HEAT = 1,
+	COOL = 2,
+	AUTO = 3,
+}
+
 export class ThermostatService implements IThermostatService {
 
 	private service: hap.Service;
 
 	private currentTemp: Characteristic<number>;
 	private targetTemp: Characteristic<number>;
+	private targetCoolingHeatingState: Characteristic<TargetHeatingCoolingState>;
 
 	constructor(name: string) {
 		this.service = new hap.Service.Thermostat(name);
 		this.currentTemp = new Characteristic(this.service, hap.Characteristic.CurrentTemperature, DEFAULT_CURRENT_TEMP);
 		this.targetTemp = new Characteristic(this.service, hap.Characteristic.TargetTemperature, DEFAULT_TARGET_TEMP);
+		this.targetCoolingHeatingState = new Characteristic(
+			this.service,
+			hap.Characteristic.TargetHeatingCoolingState,
+			TargetHeatingCoolingState.OFF,
+		);
+
+		this.init();
 	}
 
 	public getService(): hap.Service {
@@ -36,5 +51,9 @@ export class ThermostatService implements IThermostatService {
 
 	public onTargetTempChange(callback: () => void) {
 		this.targetTemp.onChange(callback);
+	}
+
+	private init() {
+		this.targetCoolingHeatingState.setValue(TargetHeatingCoolingState.AUTO);
 	}
 }
